ac70e8cfa70b33d9eebd4f24f47fa0492cc9e5e0
yakhousam <42283612+yakhousam@users.noreply.github.com>
2023-12-06 23:29:35 +0100

    feat: add get all clients route

--- server/src/routes/client.ts
+++ server/src/routes/client.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
-import Client from '../controllers/client'
+import ClientController from '../controllers/client'
 
 const clientRoute = Router()
 
+clientRoute.get('/clients', (req, res) => {
+  void ClientController.find(req, res)
+})
+
 clientRoute.post('/clients/create', (req, res) => {
-  void Client.create(req, res)
+  void ClientController.create(req, res)
 })
 
 export default clientRoute
