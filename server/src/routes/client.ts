8ca8e45ff4c111faccc0dab5ff925601f6ee7073
yakhousam <42283612+yakhousam@users.noreply.github.com>
2023-12-08 19:53:39 +0100

    rf client route, use next

--- server/src/routes/client.ts
+++ server/src/routes/client.ts
@@ -3,24 +3,24 @@ import ClientController from '@/controllers/client'
 
 const clientRoute = Router()
 
-clientRoute.get('/clients', (req, res) => {
-  void ClientController.find(req, res)
+clientRoute.get('/clients', (req, res, next) => {
+  void ClientController.find(req, res, next)
 })
 
-clientRoute.get('/clients/:id', (req, res) => {
-  void ClientController.findById(req, res)
+clientRoute.get('/clients/:id', (req, res, next) => {
+  void ClientController.findById(req, res, next)
 })
 
-clientRoute.post('/clients/create', (req, res) => {
-  void ClientController.create(req, res)
+clientRoute.post('/clients/create', (req, res, next) => {
+  void ClientController.create(req, res, next)
 })
 
-clientRoute.put('/clients/update/:id', (req, res) => {
-  void ClientController.update(req, res)
+clientRoute.put('/clients/update/:id', (req, res, next) => {
+  void ClientController.update(req, res, next)
 })
 
-clientRoute.delete('/clients/delete/:id', (req, res) => {
-  void ClientController.delete(req, res)
+clientRoute.delete('/clients/delete/:id', (req, res, next) => {
+  void ClientController.delete(req, res, next)
 })
 
 export default clientRoute
