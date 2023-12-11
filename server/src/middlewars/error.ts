727cbeef18ee6457fa08fb44932083847a45f461
yakhousam <42283612+yakhousam@users.noreply.github.com>
2023-12-11 15:16:17 +0100

    mend

--- server/src/middlewars/error.ts
+++ server/src/middlewars/error.ts
@@ -3,18 +3,16 @@ import { Error as MongooseError } from 'mongoose'
 
 import { type Request, type Response } from 'express'
 
-function errorMiddleware (error: Error, req: Request, res: Response): void {
+function errorMiddleware(error: Error, req: Request, res: Response): void {
   if (error instanceof ZodError) {
     res
       .status(400)
       .json({ errors: error.flatten().fieldErrors, message: 'Missing Fields.' })
   } else if (error instanceof MongooseError.CastError) {
-    res
-      .status(400)
-      .json({
-        error: `${error.path}`,
-        message: `${error.value} : is not a valid id`
-      })
+    res.status(400).json({
+      error: `${error.path}`,
+      message: `${error.value} : is not a valid id`
+    })
   } else {
     res.status(500).end()
   }
