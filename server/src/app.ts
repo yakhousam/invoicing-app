fa78d646c28670bbecf10ec8ef65b14f981b7e62
yakhousam <42283612+yakhousam@users.noreply.github.com>
2023-12-28 17:16:32 +0100

    Add CORS middleware for cross-origin resource sharing

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,18 @@
 import errorMiddleware from '@/middlewars/error'
 import rootRouter from '@/routes'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 import express, { type Application } from 'express'
 import http from 'http'
 
 const app: Application = express()
 
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  })
+)
 app.use(cookieParser())
 app.use(express.json())
 app.use('/api/v1', rootRouter)
