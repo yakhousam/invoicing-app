971e76a814966ef68cd842b4b4ff19a3352c6338
yakhousam <42283612+yakhousam@users.noreply.github.com>
2024-05-13 09:58:14 +0100

    add  Breadcrumbs component

--- client/src/components/Breadcrumbs.tsx
+++ client/src/components/Breadcrumbs.tsx
@@ -1,18 +1,49 @@
-import { useRouter } from '@tanstack/react-router'
+import { Breadcrumbs as MuiBreadcrumbs, Typography } from '@mui/material'
+import { Link as RouterLink, useRouter } from '@tanstack/react-router'
 
 const Breadcrumbs = () => {
   const router = useRouter()
+  const pathList = router.state.matches
+    .at(-1)
+    ?.pathname.split('/')
+    .filter(Boolean)
 
-  const breadcrumbs = router.state.matches.map((match) => {
-    console.log('match', match)
-    // const { routeContext } = match
-    // return {
-    //   title: routeContext.getTitle(),
-    //   path: match.path
-    // }
-    return match.pathname
-  })
-  return breadcrumbs
+  if (!pathList) {
+    return null
+  }
+
+  if (pathList.length < 1) {
+    return <Typography>Dashboard</Typography>
+  }
+
+  return (
+    <MuiBreadcrumbs
+      sx={{
+        '& a': {
+          textDecoration: 'none',
+          color: 'inherit'
+        },
+        '& a:hover': {
+          textDecoration: 'underline'
+        }
+      }}
+    >
+      <RouterLink to="/">
+        <Typography>Dashboard</Typography>
+      </RouterLink>
+      {pathList.slice(0, -1).map((path) => {
+        return (
+          <RouterLink key={path} to={`/${path}`}>
+            <Typography sx={{ textTransform: 'capitalize' }}>{path}</Typography>
+          </RouterLink>
+        )
+      })}
+
+      <Typography sx={{ textTransform: 'capitalize' }}>
+        {pathList?.at(-1) || 'Dashboard'}
+      </Typography>
+    </MuiBreadcrumbs>
+  )
 }
 
 export default Breadcrumbs
