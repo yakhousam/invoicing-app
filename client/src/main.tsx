2d8c3933c148a9773cf0afb09174d5c333c17ad1
yakhousam <42283612+yakhousam@users.noreply.github.com>
2024-05-17 17:53:26 +0100

    chore: Update index.css and main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 
 import App from './App'
 import { AuthProvider } from './auth'
