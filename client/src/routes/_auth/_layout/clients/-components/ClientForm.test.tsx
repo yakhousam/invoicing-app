42233cb5daefdcb57146acf8af91a096ba669ee4
yakhousam <42283612+yakhousam@users.noreply.github.com>
2024-05-27 07:18:30 +0100

    chore: Add more tests ClientForm

--- client/src/routes/_auth/_layout/clients/-components/ClientForm.test.tsx
+++ client/src/routes/_auth/_layout/clients/-components/ClientForm.test.tsx
@@ -43,6 +43,63 @@ describe('ClientForm', () => {
     expect(screen.getByLabelText(/email/i)).toHaveTextContent('')
     expect(screen.getByLabelText(/address/i)).toHaveTextContent('')
   })
+
+  it('create client with only a name (no email or address)', async () => {
+    render(<ClientForm />, {
+      wrapper: Wrapper
+    })
+    const { name } = generateClient()
+    await userEvent.type(screen.getByLabelText(/name/i), name)
+    await userEvent.click(screen.getByRole('button', { name: /create/i }))
+    // check that the user sees a success message
+    await waitFor(() => {
+      expect(screen.getByRole('alert')).toBeInTheDocument()
+      expect(screen.getByRole('alert')).toHaveTextContent(/client created/i)
+    })
+    // test that the form is cleared after submission
+    expect(screen.getByLabelText(/name/i)).toHaveTextContent('')
+    expect(screen.getByLabelText(/email/i)).toHaveTextContent('')
+    expect(screen.getByLabelText(/address/i)).toHaveTextContent('')
+  })
+
+  it('create client with name and email  but no address ', async () => {
+    render(<ClientForm />, {
+      wrapper: Wrapper
+    })
+    const { name, email } = generateClient()
+    await userEvent.type(screen.getByLabelText(/name/i), name)
+    await userEvent.type(screen.getByLabelText(/email/i), email)
+    await userEvent.click(screen.getByRole('button', { name: /create/i }))
+    // check that the user sees a success message
+    await waitFor(() => {
+      expect(screen.getByRole('alert')).toBeInTheDocument()
+      expect(screen.getByRole('alert')).toHaveTextContent(/client created/i)
+    })
+    // test that the form is cleared after submission
+    expect(screen.getByLabelText(/name/i)).toHaveTextContent('')
+    expect(screen.getByLabelText(/email/i)).toHaveTextContent('')
+    expect(screen.getByLabelText(/address/i)).toHaveTextContent('')
+  })
+
+  it('create client with name and address but no email', async () => {
+    render(<ClientForm />, {
+      wrapper: Wrapper
+    })
+    const { name, address } = generateClient()
+    await userEvent.type(screen.getByLabelText(/name/i), name)
+    await userEvent.type(screen.getByLabelText(/address/i), address)
+    await userEvent.click(screen.getByRole('button', { name: /create/i }))
+    // check that the user sees a success message
+    await waitFor(() => {
+      expect(screen.getByRole('alert')).toBeInTheDocument()
+      expect(screen.getByRole('alert')).toHaveTextContent(/client created/i)
+    })
+    // test that the form is cleared after submission
+    expect(screen.getByLabelText(/name/i)).toHaveTextContent('')
+    expect(screen.getByLabelText(/email/i)).toHaveTextContent('')
+    expect(screen.getByLabelText(/address/i)).toHaveTextContent('')
+  })
+
   describe('Form validation', () => {
     it('show error on duplicate name', async () => {
       server.use(
