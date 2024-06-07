f7f54cf73c18107c9971b7ac3a857eb5f36865b5
yakhousam <42283612+yakhousam@users.noreply.github.com>
2024-06-07 14:36:24 +0100

    feat: Add UserInfos component for updating user information
    A new component called UserInfos has been added to allow users to update their personal information. This component includes fields for name and email, and a save button to submit the changes. It improves the user experience by providing a convenient way to update user details.

---
+++ client/src/routes/_auth/_layout/settings/-components/UserInfos.tsx
@@ -0,0 +1,97 @@
+import * as api from '@/api/user'
+import { LoadingButtonSave } from '@/components/LoadingButton'
+import RHFTextField from '@/components/RHF/RHFTextField'
+import { UpdateUser, User, updateUserSchema } from '@/validations'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Stack, Typography } from '@mui/material'
+import { useMutation } from '@tanstack/react-query'
+import { useSnackbar } from 'notistack'
+import { FormProvider, useForm } from 'react-hook-form'
+
+function UserInfos({
+  user,
+  onUpdateUser
+}: {
+  user: User
+  onUpdateUser: (user: User) => void
+}) {
+  const { enqueueSnackbar } = useSnackbar()
+  const formMethods = useForm<UpdateUser>({
+    defaultValues: {
+      name: user?.name || '',
+      email: user?.email || ''
+    },
+    resolver: zodResolver(updateUserSchema)
+  })
+  const {
+    handleSubmit,
+    formState: { isSubmitting, isDirty }
+  } = formMethods
+
+  const mutation = useMutation({
+    mutationFn: ({ data, userId }: { data: UpdateUser; userId: string }) =>
+      api.updateUser(data, userId),
+    onSuccess: (data) => {
+      enqueueSnackbar('User updated', { variant: 'success' })
+      onUpdateUser(data)
+    },
+    onError: async (error: Error | Response) => {
+      if (error instanceof Response && error.status === 409) {
+        // const data = (await error.json()) as {
+        //   error: 'DuplicateKeyError'
+        //   message: string
+        //   field: keyof CreateClient
+        //   value: string
+        // }
+        // setError(data.field, {
+        //   message: `a client with the same ${data.field} already exists: ${data.value}`
+        // })
+      } else {
+        enqueueSnackbar('Error creating client', { variant: 'error' })
+      }
+    }
+  })
+  const onSubmit = (data: UpdateUser) => {
+    mutation.mutate({ data, userId: user._id })
+  }
+  return (
+    <>
+      <Typography variant="h5">User Information</Typography>
+      <Typography variant="body2" gutterBottom>
+        Update your personal information
+      </Typography>
+      <FormProvider {...formMethods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Stack spacing={6} mt={3}>
+            <RHFTextField
+              variant="standard"
+              margin="normal"
+              fullWidth
+              label="Name"
+              name="name"
+              autoFocus
+            />
+
+            <RHFTextField
+              variant="standard"
+              margin="normal"
+              fullWidth
+              label="Email"
+              name="email"
+            />
+            <LoadingButtonSave
+              type="submit"
+              loading={isSubmitting}
+              disabled={!isDirty}
+              sx={{ alignSelf: 'flex-end' }}
+            >
+              Save
+            </LoadingButtonSave>
+          </Stack>
+        </form>
+      </FormProvider>
+    </>
+  )
+}
+
+export default UserInfos
--- client/src/routes/_auth/_layout/settings/index.tsx
+++ client/src/routes/_auth/_layout/settings/index.tsx
@@ -1,14 +1,32 @@
-import { useAuth } from '@/hooks/useAuth'
-import { Typography } from '@mui/material'
+import { Box, Paper, Typography } from '@mui/material'
 import { createFileRoute } from '@tanstack/react-router'
-import ProfileForm from './-components/ProfileForm'
+import UserInfos from './-components/UserInfos'
 
 export const Route = createFileRoute('/_auth/_layout/settings/')({
-  component: () => <Profile />
+  component: () => <Settings />
 })
 
-function Profile() {
-  const { user, setUser } = useAuth()
+function Settings() {
+  const {
+    auth: { user, setUser }
+  } = Route.useRouteContext()
   if (!user) return <Typography variant="h4">Loading...</Typography>
-  return <ProfileForm user={user} onUpdateUser={setUser} />
+  return (
+    <Wrapper>
+      <UserInfos user={user} onUpdateUser={setUser} />
+    </Wrapper>
+  )
+}
+
+function Wrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <Box
+      sx={{
+        maxWidth: (theme) => theme.breakpoints.values.sm,
+        margin: 'auto'
+      }}
+    >
+      <Paper sx={{ px: 4, py: 2 }}>{children}</Paper>
+    </Box>
+  )
 }
