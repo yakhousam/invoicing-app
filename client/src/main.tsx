e015f96a217ad84dcb6fe4b70fb2190735129426
yakhousam <42283612+yakhousam@users.noreply.github.com>
2024-08-23 06:23:51 +0100

    refactor: Update npm dependencies, including @sentry/react to version 8.26.0

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import {
   ErrorComponent,
@@ -10,6 +11,21 @@ import ReactDOM from 'react-dom/client'
 import { auth } from './auth'
 import { Spinner } from './components/spinner'
 import { routeTree } from './routeTree.gen'
+
+Sentry.init({
+  dsn: 'https://eb6f401e82cdd7d6e5e49d78db270251@o4507824956571648.ingest.de.sentry.io/4507824964042832',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration()
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
 
 const queryClient = new QueryClient()
 
